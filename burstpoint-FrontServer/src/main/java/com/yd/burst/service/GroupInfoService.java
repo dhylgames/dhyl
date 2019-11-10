fdaedcef9b61cdb03216330edfe23eb87db0e110
waylon <13593854782@163.com>
2019-11-10 17:09:35 +0800

    1 增加群信息表service 2 增加群成员表service

---
+++ burstpoint-FrontServer/src/main/java/com/yd/burst/service/GroupInfoService.java
@@ -0,0 +1,13 @@
+package com.yd.burst.service;
+
+
+
+/**
+ * @Description:
+ * @Author: Will
+ * @Date: 2019-07-29 19:50
+ **/
+public interface GroupInfoService {
+
+
+}
---
+++ burstpoint-FrontServer/src/main/java/com/yd/burst/service/impl/GroupInfoServiceImpl.java
@@ -0,0 +1,26 @@
+package com.yd.burst.service.impl;
+
+import com.yd.burst.cache.CacheBase;
+import com.yd.burst.dao.UserMapper;
+import com.yd.burst.service.GroupInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Description:
+ * @Author: Will
+ * @Date: 2019-07-29 19:51
+ **/
+@Service
+@Transactional
+public class GroupInfoServiceImpl implements GroupInfoService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private CacheBase cacheBase;
+
+}
+
