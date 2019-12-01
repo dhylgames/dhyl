60c3b5ec150666e39de68ccf57fec20ad7f4be61
bobo <879980333@qq.com>
2019-12-01 17:18:53 +0800

    添加注释

--- burstpoint-FrontServer/src/main/java/com/yd/burst/service/impl/GroupAmountServiceImpl.java
+++ burstpoint-FrontServer/src/main/java/com/yd/burst/service/impl/GroupAmountServiceImpl.java
@@ -3,12 +3,16 @@ package com.yd.burst.service.impl;
 import com.yd.burst.dao.GroupAmountMapper;
 import com.yd.burst.model.GroupAmount;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Auther: kelly
  * @Date: 2019/12/1 16:29
  * @Description:
  */
+@Service
+@Transactional
 public class GroupAmountServiceImpl implements GroupAmountService {
 
     @Autowired
