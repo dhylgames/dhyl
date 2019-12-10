fa268f97eacbdba07cc31ee085157b164ff60250
waylon <13593854782@163.com>
2019-12-10 22:53:20 +0800

    查询只有一个 但是查询结果有四条

--- burstpoint-FrontServer/src/main/java/com/yd/burst/webSkt/WebSocket.java
+++ burstpoint-FrontServer/src/main/java/com/yd/burst/webSkt/WebSocket.java
@@ -430,7 +430,7 @@ public class WebSocket implements Serializable {
         String key = getKey(CacheKey.GROUP_ROOM_KEY, groupCode, roomCode);
         List<Player> players = (List<Player>) redisPool.getData4Object2Redis(key);
         for(Player player:players){
-            if(player.getBanker()){
+            if(null!=player.getBanker()&&player.getBanker()){
                 return null; //如果已经抢过，直接返回
             }
         }
